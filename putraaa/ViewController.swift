ad4a90efe9a081707f28e488dde60de0c4d5cdcc
Rachmanto Saputra <rachmantosaputra@outlook.jp>
2020-02-10 11:23:52 +0700

    Initial Commit

---
+++ putraaa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  putraaa
+//
+//  Created by Mac on 10/02/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
